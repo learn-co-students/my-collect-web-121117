e8a6a3161c2bd1e2592e5d0a91a5ba92346ab259
github username <github email address>
2017-12-01 15:30:38 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+# write a method that works like the collect method
 
+# while loop
 
+# uses yield
+
+#
+def my_collect(arr)
+counter = 0
+y =[]
+while counter < arr.length
+  y.push yield arr[counter]
+  counter += 1
+end
+  y
+end
