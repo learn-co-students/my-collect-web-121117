a291f7fbc08e12d5d2671a58da521fd5dd9dec03
github username <github email address>
2017-12-03 16:51:50 -0800

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,10 @@
 def my_collect(array)
-   i = 0
-   return_array = []
-   
-   while i < array.length
-       return_array << yield(array[i])
-       i += 1
-    end
-   return_array
-end
+  i = 0
+  new_array = []
 
+  while i < array.length
+    new_array << yield(array[i])
+    i += 1
+  end
+  new_array
+end
